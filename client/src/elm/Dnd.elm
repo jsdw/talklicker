72f823ba8e073de1341baf7548bd306c6a5e6a43
James Wilson <me@unbui.lt>
2016-09-05 20:53:06 +0100

    tidy up DnD list building code and maintain before and after for each drop location for better feedback

--- client/src/elm/Dnd.elm
+++ client/src/elm/Dnd.elm
@@ -4,6 +4,8 @@ import Html.Events exposing (..)
 import Html.Attributes exposing (..)
 import Html exposing (..)
 
+import Array
+
 import Mouse
 import Json.Decode as JsonDec
 
@@ -13,7 +15,7 @@ import Json.Decode as JsonDec
 
 type alias Model =
     { selectedId : Maybe String
-    , dragPosition : Maybe (DragPosition String)
+    , dragPosition : Maybe DragPosition
     , dragInProgress : Bool
     , startXY : Mouse.Position
     , currentXY : Mouse.Position
@@ -42,12 +44,17 @@ exceedsThreshold posA posB =
 
 type Msg
     = DragStart String Mouse.Position
-    | DragOver (Maybe (DragPosition String))
+    | DragOver (Maybe DragPosition)
     | DragMove Mouse.Position
     | DragComplete
 
-type DragPosition id
-    = AtBefore id
+-- a tuple of the things we're between at present,
+-- which could be Ids or beginning/end of list.
+type alias DragPosition = (Position, Position)
+
+type Position
+    = AtBeginning
+    | AtId String
     | AtEnd
 
 update : Msg -> Model -> Model
@@ -87,7 +94,7 @@ sub model = case model.selectedId of
 --
 
 view : Model -> (Msg -> parentMsg) -> List (String, Html parentMsg) -> Html parentMsg
-view model pm items' =
+view model pm items =
   let
     -- drag start occurs
     onDragStart id =
@@ -98,7 +105,7 @@ view model pm items' =
     -- the element that the mouse is over has changed
     onDragOver mDragPos =
         onWithOptions
-            "mouseover"
+            "mouseenter"
             { defaultOptions | preventDefault = True }
             (JsonDec.succeed (pm <| DragOver mDragPos))
     -- this event finishes the drag successfully when called:
@@ -112,13 +119,9 @@ view model pm items' =
         Just _  -> True
         Nothing -> False
     -- wrap a list item so that we start drag onmousedown and react to drops.
-    toDndItem (id,html) mNextId =
+    toDndItem lastPos (id,html) nextPos =
       let
         isBeingDragged = Just id == model.selectedId
-        prevDragPos = AtBefore id
-        nextDragPos = case mNextId of
-            Nothing -> AtEnd
-            Just nextId -> AtBefore nextId
       in
         div [ class ("dnd-item" ++ if isBeingDragged && model.dragInProgress then " being-dragged" else "")
             , onDragStart id
@@ -128,33 +131,55 @@ view model pm items' =
             -- and otherwise keeping out of the way. Use them to figure out whether we're dragging above or
             -- below the current thing:
             , div [ class "dnd-item-before"
-                  , onDragOver (if isBeingDragged then Nothing else Just prevDragPos)
+                  , onDragOver (if isBeingDragged then Nothing else Just (lastPos, AtId id))
                   , completeOnMouseUp
                   ] []
             , div [ class "dnd-item-after"
-                  , onDragOver (if isBeingDragged then Nothing else Just nextDragPos)
+                  , onDragOver (if isBeingDragged then Nothing else Just (AtId id, nextPos))
                   , completeOnMouseUp
                   ] []
             ]
     -- these live between each item (and at the beginning and end) and expand when the drag is in their
     -- region or collapse otherwise, to make space for the drop.
-    -- toDndSpacer : DragPosition String -> Html parentMsg
-    toDndSpacer dragPosition =
-        div [ class ("dnd-spacer" ++ if model.dragPosition == Just dragPosition then " active" else "")
+    toDndSpacer ((a,b) as dragPosition) =
+      let
+        mDragId = Maybe.map AtId model.selectedId
+        isNearDragged = Just a == mDragId || Just b == mDragId
+      in
+        div [ class
+                (  "dnd-spacer"
+                ++ (if model.dragPosition == Just dragPosition then " active" else "")
+                ++ (if isNearDragged then " near-dragged" else "")
+                )
             , onDragOver (Just dragPosition)
             , completeOnMouseUp
             ] []
-    -- run through our items, inserting spacers and wrapped items in a list:
-    -- dndItems : List (String, Html parentMsg) -> List (Html parentMsg)
-    dndItems items = case items of
-        [] ->
-            [ toDndSpacer AtEnd ]
-        ((lId, _) as last) :: [] ->
-            toDndSpacer (AtBefore lId) :: toDndItem last Nothing :: dndItems []
-        ((fId, _) as first) :: ((sId,_) as second) :: rest ->
-            toDndSpacer (AtBefore fId) :: toDndItem first (Just sId) :: dndItems (second :: rest)
+    -- run through our items, inserting spacers between items, at the beginning and end,
+    -- and wrapping items into dndItems:
+    dndItems itemArr =
+      let
+        fn (idx,(itemId, itemHtml)) output =
+          let
+            lastPos = defaultFromArr (idx-1) (AtId << fst) AtBeginning itemArr
+            nextPos = defaultFromArr (idx+1) (AtId << fst) AtEnd itemArr
+
+            item1 rest = if lastPos == AtBeginning then toDndSpacer (AtBeginning,AtId itemId) :: rest else rest
+            item2 rest = toDndItem lastPos (itemId, itemHtml) nextPos :: rest
+            item3 rest = toDndSpacer (AtId itemId, nextPos) :: rest
+          in
+            item1 <| item2 <| item3 <| output
+      in
+        case Array.isEmpty itemArr of
+            False -> Array.foldr fn [] (Array.indexedMap (\i a -> (i,a)) itemArr)
+            True  -> [toDndSpacer (AtBeginning, AtEnd)]
   in
+    -- our output is a dnd-items div wrapping spacer-separated draggable items:
     div [ class ("dnd-items" ++ if isDrag then " active" else "")
         , onMouseLeave (pm (DragOver Nothing))
         ]
-        (dndItems items')
+        (dndItems (Array.fromList items))
+
+defaultFromArr : Int -> (a -> b) -> b -> Array.Array a -> b
+defaultFromArr idx fn def a = case Array.get idx a of
+    Nothing -> def
+    Just v -> fn v
